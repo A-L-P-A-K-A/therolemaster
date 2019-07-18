3509c931d4c077733d9d40be4b1740ce38dbf210
ALPAKA <40245067+A-L-P-A-K-A@users.noreply.github.com>
2019-07-19 01:09:50 +1000

    Update role.js

--- role.js
+++ role.js
@@ -1,7 +1,3 @@
-require('dotenv').config()
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
 // Configuration & Settings
 const yourID = "395397757717708800"; //Instructions on how to get this: https://redd.it/40zgse
 const setupCMD = "!rolesetup";
